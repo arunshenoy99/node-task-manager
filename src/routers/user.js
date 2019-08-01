35b2fad46cc81cc3903fe10fba6d206a8f4fac69
Arun Shenoy <devarunshenoy99@gmail.com>
2019-08-01 17:24:33 +0530

    added image support in mongoDb

--- src/routers/user.js
+++ src/routers/user.js
@@ -99,7 +99,7 @@ router.delete('/users/me',auth,async(req,res)=>{
 })
 
 const upload = multer({
-    dest:'avatars',
+    // dest:'avatars',                                  //REMOVE DESTINATION TO PASS FILE TO ROUTER FUNCTION(req.file)
     limits:{
         fileSize:1000000          //1 MB 
     },
@@ -115,8 +115,31 @@ const upload = multer({
    
 })
 
-router.post('/users/me/avatar',upload.single('avatar'),(req , res)=>{
+router.post('/users/me/avatar',auth,upload.single('avatar'),async(req , res)=>{
+    req.user.avatar=req.file.buffer                                     //BINARY DATA OF FILE
+    await req.user.save()
     res.send()
+},(error,req,res,next)=>{
+    res.status(400).send({error:error.message})
+})
+
+router.delete('/users/me/avatar',auth,async(req,res)=>{
+    req.user.avatar=undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar',async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+        if(!user||!user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type','image/jpg')
+        res.send(user.avatar)
+    }catch(e){
+        res.status(404).send()
+    }
 })
 
 
